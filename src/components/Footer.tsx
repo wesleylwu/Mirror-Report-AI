62fe6490175991a2630fbcd5fc9719d799b41a8c
wesley <wesleylwu@gmail.com>
2026-06-25 10:59:34 +0900

    upload feature

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,17 @@
 
 const Footer = () => {
   return (
-    <div className="text-mirror-white bg-mirror-dark-blue mt-auto w-full shadow-md transition-all duration-300 select-none">
-      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
+    <div className="text-mirror-white bg-mirror-dark-blue mt-auto w-full shadow-md select-none">
+      <div className="mx-auto max-w-[80vw]" style={{ padding: "3vh 2vw" }}>
+        <div
+          className="flex flex-col items-center justify-between sm:flex-row"
+          style={{ gap: "2vh" }}
+        >
           <div className="flex items-center">
             <p className="text-mirror-white text-lg font-bold">smartNexus®</p>
           </div>
           <div className="flex items-center">
-            <p className="text-mirror-cyan-footer/80 text-sm">
+            <p className="text-mirror-light-blue/80 text-sm">
               (C) 2026 Suncreer All rights Reserved.
             </p>
           </div>
